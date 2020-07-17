39ea9e2d947a39a6ec7d2a3be03af939024948b1
ssjisan <sakibfset13@gmail.com>
2020-07-17 19:29:02 +0600

    final

--- assignment.js
+++ assignment.js
@@ -1,29 +1,46 @@
 // Feet To Miles 
 
 function feetToMile (feet){
-    var mile= feet*0.000189;
-    return mile;
+    if(feet < 0 ){
+    return "Distance Cannot Be Negative";    
 }
-var answer = feetToMile(20000);
+    else{
+        var mile= feet*0.000189;
+        return mile;
+    }
+    
+}
+var answer = feetToMile();
 console.log(answer);
 
 //  Wood Calculator
 
 function woodCalculator(chair,table,bed){
-    var woodForChair=   chair*1;
-    var woodForTable=   table*3;
-    var woodForBed=     bed*5;
-    var total= woodForChair + woodForTable + woodForBed;
-    return total;
+    if (chair<0 || table<0 || bed<0){
+        return "Enter Correct Ammount";
+    }
+    else{
+        var woodForChair=   chair*1;
+        var woodForTable=   table*3;
+        var woodForBed=     bed*5;
+        var total= woodForChair + woodForTable + woodForBed;
+        return total;
+    }
+       
+
 }
-var answer = woodCalculator(1,1,1) ;
-console.log("You need total" ,answer,"cubic feet wood.");
+var answer = woodCalculator() ;
+console.log(answer);
 
 
 // Brick Calculator
 
 function brickCalculator(floor){
-    var firstToTen = (floor*15)*1000;
+    if(floor <= 0){
+        console.log("Enter Something Valid");
+    }
+    else{
+        var firstToTen = (floor*15)*1000;
     var elevenToTwenty = (floor*12)*1000;
     var upToTwenty = (floor*10)*1000;
     if(floor<=10)
@@ -32,12 +49,18 @@ function brickCalculator(floor){
         console.log("You need Total", elevenToTwenty, "bricks");
     else if(floor>20)
         console.log("You need Total", upToTwenty, "bricks");
+    }
+    
 }
-var result= brickCalculator(122) ;
+var result= brickCalculator() ;
 
 // Tiny Friend
 
 function tinyFriend(name){
+    if(name== ""){
+        return "Enter Something";
+    }
+    else{
         var myFriends = name[0];
         for(var i = 0; i < name.length; i++){
             var currentName = name[i];
@@ -47,6 +70,8 @@ function tinyFriend(name){
         }
         return myFriends;
     }
+       
+    }
 
-var myTinyFriend = tinyFriend(["Saab", "M", "Ferrari", "Volvo", "BMW"]);
-console.log(resultmyTinyFriend);
+var myTinyFriend = tinyFriend([]);
+console.log(myTinyFriend);
